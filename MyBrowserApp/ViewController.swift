46ee6714a328d428c96ad179c6a94c6c5daab7a8
Takeyuki UEDA <ueda@latelierdueda.com>
2019-01-04 19:32:13 +0900

    #05

--- MyBrowserApp/ViewController.swift
+++ MyBrowserApp/ViewController.swift
@@ -8,13 +8,30 @@
 
 import UIKit
 
-class ViewController: UIViewController {
 
+class ViewController: UIViewController {
+    @IBOutlet weak var urlTextField: UITextField!
+    @IBOutlet weak var browserWebView: UIWebView!
+    @IBOutlet weak var backButton: UIBarButtonItem!
+    @IBOutlet weak var forwardButton: UIBarButtonItem!
+    @IBOutlet weak var reloadButton: UIBarButtonItem!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let urlString = "http://dotinstall.com"
+        let urlRequest = URLRequest(url: URL(string: urlString)!)
+        self.browserWebView.loadRequest(urlRequest)
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func goBack(_ sender: Any) {
+    }
+    
+    @IBAction func goForward(_ sender: Any) {
+    }
+    
+    @IBAction func reload(_ sender: Any) {
+    }
 }
 
