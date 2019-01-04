5556ead4982120e29b8aeb217db9e219de33510d
Takeyuki UEDA <ueda@latelierdueda.com>
2019-01-04 16:25:21 +0900

    Initial Commit

---
+++ MyBrowserApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyBrowserApp
+//
+//  Created by 上田健之 on 2019/01/04.
+//  Copyright © 2019 Takeyuki UEDA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ MyBrowserAppTests/MyBrowserAppTests.swift
@@ -0,0 +1,34 @@
+//
+//  MyBrowserAppTests.swift
+//  MyBrowserAppTests
+//
+//  Created by 上田健之 on 2019/01/04.
+//  Copyright © 2019 Takeyuki UEDA. All rights reserved.
+//
+
+import XCTest
+@testable import MyBrowserApp
+
+class MyBrowserAppTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
